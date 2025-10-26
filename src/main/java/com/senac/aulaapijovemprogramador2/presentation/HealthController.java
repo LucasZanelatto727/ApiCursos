3ed1ea39edcdffdc3b9acf0706c4907f2ea0ebcd
lucas <lucaszanelatto64@gmail.com>
2025-10-26 10:30:14 -0300

    Adicionando novo teste no CI/CD.

--- src/main/java/com/senac/aulaapijovemprogramador2/presentation/HealthController.java
+++ src/main/java/com/senac/aulaapijovemprogramador2/presentation/HealthController.java
@@ -1,4 +1,16 @@
 package com.senac.aulaapijovemprogramador2.presentation;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/presentation/HealthController")
 public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("API está saudável ✅");
+    }
 }
