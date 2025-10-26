645a857857ffda0c27d15180d4b71501fc87cb82
lucas <lucaszanelatto64@gmail.com>
2025-10-26 11:18:46 -0300

    Adicionando novo teste no CI/CD.

--- src/main/java/com/senac/aulaapijovemprogramador2/presentation/HealthController.java
+++ src/main/java/com/senac/aulaapijovemprogramador2/presentation/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/presentation/HealthController")
+@RequestMapping("/api/health")
 public class HealthController {
 
     @GetMapping
